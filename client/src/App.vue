09313f905fba555cf6e23f4191238abe05778dc3
ArnoldPires <arnold.pires92@gmail.com>
2024-02-06 21:02:29 -0500

    Created the chatroom, changed room names, and a bunch of other stuff

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <v-app>
-    <v-main>
+    <v-main class="mt-14">
       <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts" setup>
-  //
+//
 </script>
--- server/utils.js
+++ server/utils.js
@@ -3,6 +3,12 @@ import moment from "moment";
 export const botName = "Vue Chatapp Admin";
 export const users = [];
 
+/**
+ * Adds a new user to the chat.
+ * @param {Object} user - The user object to add.
+ * @returns {Object} The added user object.
+ */
+
 // Join Chat
 export function userJoin(user) {
   users.push(user);
@@ -26,7 +32,9 @@ export function userLeave(id) {
   if (index !== -1) {
     return users.splice(index, 1)[0];
   }
+  return null; // Explicitly indicate no user was removed.
 }
+
 export function getCurrentUser(id) {
   return users.find((user) => user.id === id);
 }
